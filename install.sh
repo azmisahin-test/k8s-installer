2c889a5895b113eadee8e59df8a91176a7d813a5
Azmi SAHIN <azmisahin@outlook.com>
2024-07-24 22:28:38 +0300

    kubernetes-bionic

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ sudo systemctl enable docker
 
 echo -e "${GREEN}Adım 3: Kubernetes Kurulumu${NC}"
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-bionic main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
