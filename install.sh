546edb8a4af4661476b384c4ad55d3efb84aca6f
Azmi SAHIN <azmisahin@outlook.com>
2024-07-24 22:47:11 +0300

    delete old kubernetes repo list

--- install.sh
+++ install.sh
@@ -12,7 +12,13 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y apt-transport-https ca-certificates curl
 
-echo -e "${GREEN}Adım 2: Docker Kurulumu${NC}"
+echo -e "${GREEN}Adım 2: Eski Kubernetes Depo Dosyasını Silme${NC}"
+if [ -f /etc/apt/sources.list.d/kubernetes.list ]; then
+    echo -e "${GREEN}Eski Kubernetes depo dosyası bulundu, siliniyor...${NC}"
+    sudo rm /etc/apt/sources.list.d/kubernetes.list
+fi
+
+echo -e "${GREEN}Adım 3: Docker Kurulumu${NC}"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -23,22 +29,22 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-echo -e "${GREEN}Adım 3: Kubernetes Kurulumu${NC}"
+echo -e "${GREEN}Adım 4: Kubernetes Kurulumu${NC}"
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-bionic main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo -e "${GREEN}Adım 4: Kubernetes Kümesini Başlatma${NC}"
+echo -e "${GREEN}Adım 5: Kubernetes Kümesini Başlatma${NC}"
 sudo kubeadm init
 
-echo -e "${GREEN}Adım 5: Kullanıcı Konfigürasyonu${NC}"
+echo -e "${GREEN}Adım 6: Kullanıcı Konfigürasyonu${NC}"
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo -e "${GREEN}Adım 6: Pod Network Eklenmesi${NC}"
+echo -e "${GREEN}Adım 7: Pod Network Eklenmesi${NC}"
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 echo -e "${GREEN}Kubernetes Kümesi Kurulumu Tamamlandı!${NC}"
