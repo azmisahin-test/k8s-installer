cfde45b86ce1151d4150bb8e109634ec93348d97
Azmi SAHIN <azmisahin@outlook.com>
2024-07-25 19:21:42 +0300

    fix: docker install and kubernetes repository

--- install.sh
+++ install.sh
@@ -17,7 +17,11 @@ os=$(lsb_release -is)
 if [ "$os" = "Ubuntu" ] || [ "$os" = "Debian" ]; then
   package_manager="apt"
   update_command="sudo apt update"
-  docker_install_command="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io"
+  docker_install_command="sudo apt install -y apt-transport-https ca-certificates curl software-properties-common && \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg && \
+    echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+    sudo apt update && \
+    sudo apt install -y docker-ce docker-ce-cli containerd.io"
 elif [ "$os" = "CentOS" ]; then
   package_manager="yum"
   update_command="sudo yum update"
@@ -54,10 +58,8 @@ fi
 
 # Add Kubernetes repository
 echo -e "${GREEN}Step 4: Adding Kubernetes Repository${NC}"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo tee /etc/apt/sources.list.d/kubernetes.list <<EOF
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo $update_command
 
 # Install Kubernetes components
